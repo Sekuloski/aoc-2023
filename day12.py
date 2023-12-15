14512702caa19fa0a4808d14728d22a38f227249
Bojan Sekuloski <79922210+Sekuloski@users.noreply.github.com>
2023-12-15 14:28:35 +0100

    Day 12 (part 1 - using dynamic programming)

--- day12.py
+++ day12.py
@@ -9,75 +9,75 @@ import numpy
 from aocd import get_data
 from aocd.post import submit
 
-
-def count_combinations(conditions: str, status: str) -> int:
-    result_needed = list(map(int, status.split(',')))
-    total_broken = sum(result_needed)
-    known_broken = conditions.count('#')
-    unknown = conditions.count('?')
-    need_to_put_periods = unknown - (total_broken - known_broken)
-    need_to_put_broken = total_broken - known_broken
-
-    replacement_one = '#'
-    replacement_two = '.'
-    need_to_put = need_to_put_broken
-
-    if 0 < need_to_put_periods < need_to_put_broken:
-        replacement_one = '.'
-        replacement_two = '#'
-        need_to_put = need_to_put_periods
-
-    indices = [index for index in list(range(len(conditions))) if conditions[index] == '?']
-    index_combinations: combinations = combinations(indices, need_to_put)
-
-    count = 0
-    for combo in index_combinations:
-        temp_string = numpy.array(list(conditions))
-
-        for index in combo:
-            temp_string[index] = replacement_one
-
-        result = [broken.count('#') for broken in ''.join(temp_string).replace('?', replacement_two).split('.') if broken]
-
-        if result_needed == result:
-            count += 1
-
-    return count
+states = {}
 
 
-def process_line(line: str) -> float:
-    conditions, status = line.split(' ')
-    result_one: int = count_combinations(conditions, status)
+def process(sequence, blocks, position=0, block=0, damaged=0) -> int:
+    if (position, block, damaged) in states:
+        return states[position, block, damaged]
 
-    if conditions[-1] == '#' or conditions[-1] == '?':
-        conditions: str = '?'.join(repeat(conditions, 2))
-        status: str = ','.join(repeat(status, 2))
-        result_two: int = count_combinations(conditions, status)
-        print('Done')
-        return ((result_two / result_one) ** 4) * result_one
+    # On end of sequence
+    if position == len(sequence):
+        # No more blocks -> ###.
+        if block == len(blocks) and damaged == 0:
+            return 1
+        # On last block -> ###
+        if block == len(blocks) - 1 and damaged == blocks[block]:
+            return 1
 
+        return 0
+
+    result = 0
+    # On '#'
+    if sequence[position] == '#':
+        result = process(sequence, blocks, position + 1, block, damaged + 1)
+
+    # On '.'
+    elif sequence[position] == '.':
+        # ##. [(2), 3]
+        if damaged > 0:
+            if block < len(blocks) and damaged == blocks[block]:
+                result = process(sequence, blocks, position + 1, block + 1, 0)
+        # ..
+        else:
+            result = process(sequence, blocks, position + 1, block, 0)
+
+    # On '?'
     else:
-        conditions: str = '?' + conditions
-        end_result: int = count_combinations(conditions, status)
-        print('Done')
-        return (end_result ** 4) * result_one
+        # -> ##? [(2), 3]
+        if damaged > 0:
+            if block < len(blocks) and damaged == blocks[block]:
+                result = process(sequence, blocks, position + 1, block + 1, 0)
+            else:
+                result = process(sequence, blocks, position + 1, block, damaged + 1)
+
+        # -> .?
+        else:
+            if block == len(blocks):
+                result = process(sequence, blocks, position + 1, block, 0)
+            else:
+                result = process(sequence, blocks, position + 1, block, damaged + 1) + process(sequence, blocks, position + 1, block, 0)
+
+    states[(position, block, damaged)] = result
+    return result
 
 
 def main():
-    data = get_data().split('\n')
+    data = get_data(day=12).split('\n')
     # data = open('test').read().split('\n')
-
-    start_time = time.time()
 
     answer = 0
 
-    with multiprocessing.Pool() as p:
-        for result in p.map(process_line, reversed(data)):
-            answer += result
+    for line in data:
+        states.clear()
+        sequence, blocks = line.split(' ')
+        blocks = list(map(int, blocks.split(',')))
+        result = process(sequence, blocks, damaged=0)
+        answer += result
+        print(sequence, blocks, result)
 
     print(int(answer))
-    print("--- %s seconds ---" % (time.time() - start_time))
-    # submit(int(answer))
+    submit(int(answer))
 
 
 if __name__ == '__main__':
