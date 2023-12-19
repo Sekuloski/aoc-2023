98534e1ca1f73d3b56f67a61f193479fec40d77b
Sekuloski <sekuloski.bojan18@gmail.com>
2023-12-19 08:34:54 +0100

    Day 19 - Part two progress

--- day19.py
+++ day19.py
@@ -4,12 +4,44 @@ from aocd import get_data
 from aocd.post import submit
 
 
+def process_rule(rule, rules_dict, current_path):
+    if rule == 'A':
+        return True, current_path
+
+    if rule == 'R':
+        return (False,)
+
+    print(rule, rules_dict, current_path)
+    if not isinstance(rule, tuple):
+        new_path = current_path.copy()
+        new_path.append(rule)
+        new_rules = rules_dict[rule]
+        for new_rule in new_rules:
+            return process_rule(new_rule, rules_dict, new_path)
+
+    # else:
+    #     match rule[0][1]:
+    #         case '>':
+    #             if numbers[rule[0][0]] > int(rule[0].split('>')[-1]):
+    #                 current_rule = rule[1]
+    #                 break
+    #         case '<':
+    #             if numbers[rule[0][0]] < int(rule[0].split('<')[-1]):
+    #                 current_rule = rule[1]
+    #                 break
+    #         case _:
+    #             if numbers[rule[0][0]] == int(rule[0].split('=')[-1]):
+    #                 current_rule = rule[1]
+    #                 break
+    return (False,)
+
 def main():
-    data = get_data().split('\n')
-    # data = open('test').read().split('\n')
+    # data = get_data().split('\n')
+    data = open('test').read().split('\n')
 
     answer = 0
     rules_dict = defaultdict(list)
+    possible_scenarios = []
     rules_found = False
     for line in data:
         if not line:
@@ -26,61 +58,17 @@ def main():
                 else:
                     rules_dict[rule_name].append(rule)
 
-        else:
-            parts = line.removesuffix('}').removeprefix('{').split(',')
-            numbers = {'x': int(parts[0].split('=')[1]),
-                       'm': int(parts[1].split('=')[1]),
-                       'a': int(parts[2].split('=')[1]),
-                       's': int(parts[3].split('=')[1])}
+    for rule in rules_dict['in']:
+        results = process_rule(rule, rules_dict, [])
+        if results[0]:
+            possible_scenarios.append(results[1])
 
-            current_rule = 'in'
-            exit_loop = False
-            print(numbers)
-            while True:
-                if exit_loop:
-                    break
-
-                if current_rule == 'A':
-                    answer += sum(numbers.values())
-                    break
-
-                if current_rule == 'R':
-                    break
-
-                for rule in rules_dict[current_rule]:
-                    print(rule)
-                    if not isinstance(rule, tuple):
-                        print(rule)
-                        if rule == 'R':
-                            exit_loop = True
-                            break
-
-                        elif rule == 'A':
-                            answer += sum(numbers.values())
-                            exit_loop = True
-                            break
-
-                        else:
-                            current_rule = rule
-                            break
-
-                    else:
-                        match rule[0][1]:
-                            case '>':
-                                if numbers[rule[0][0]] > int(rule[0].split('>')[-1]):
-                                    current_rule = rule[1]
-                                    break
-                            case '<':
-                                if numbers[rule[0][0]] < int(rule[0].split('<')[-1]):
-                                    current_rule = rule[1]
-                                    break
-                            case _:
-                                if numbers[rule[0][0]] == int(rule[0].split('=')[-1]):
-                                    current_rule = rule[1]
-                                    break
+                # for rule in rules_dict[current_rule]:
+                #     print(rule)
+                #
 
     print(answer)
-    submit(answer)
+    # submit(answer)
 
 
 if __name__ == '__main__':
